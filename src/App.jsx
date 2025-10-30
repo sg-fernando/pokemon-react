60fdef6f418a24ac3cfbe5269579bac52bb1ef2d
sg-fernando <45243662+sg-fernando@users.noreply.github.com>
2025-10-30 10:16:27 -0600

    cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,100 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
+import './pokeTypes.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemonName, setPokemonName] = useState("");
+  const [pokemonList, setPokemonList] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const replace = (event, index, name) => {
+        event.preventDefault();
+        console.log(`The name you entered was: ${name} at index ${index}`)
+        let url = "https://pokeapi.co/api/v2/pokemon/" + name;
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+                let name = data.name;
+                let img = data.sprites.front_default;
+                let type = data.types[0].type.name;
+                let moves = data.moves ?? [];
+                let stats = data.stats;
+                let newPokemonList = [...pokemonList];
+                newPokemonList[index] = {
+                                            name: name,
+                                            img: img,
+                                            type: type,
+                                            moves: moves,
+                                            hp: stats[0].base_stat
+                                        };
+                setPokemonList(newPokemonList);
+            });
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        let url = "https://pokeapi.co/api/v2/pokemon/" + pokemonName;
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                let name = data.name;
+                let img = data.sprites.front_default;
+                let type = data.types[0].type.name;
+                let moves = data.moves ?? [];
+                let stats = data.stats;
+                setPokemonList([
+                    ...pokemonList,
+                    {
+                            name: name,
+                            img: img,
+                            type: type,
+                            moves: moves,
+                            hp: stats[0].base_stat
+                    }
+                ]);
+            });
+    }
+
+    return (
+        <div className="App">
+        <h1>Pokemon Cards</h1>
+            <form onSubmit={handleSubmit} id="pokemon-form">
+                <input className="pokemon-name" type="text" name="pokemon" placeholder="Enter a Pokemon name..." value={pokemonName} onChange={(e) => setPokemonName(e.target.value)} /> <br />
+                <input className="submit" type="submit" value="Enter" />
+            </form>
+            <div className="pokemon-container">
+                {
+                    pokemonList.map((pokemonObj, index) => {
+                        return <Pokemon key={index} pokemonObj={pokemonObj} index={index} replace={replace} />;
+                    })
+                }
+            </div>
+        </div>
+    );
 }
 
-export default App
+function Pokemon(props) {
+    const pokemon = props.pokemonObj;
+    const [pokemonName, setPokemonName] = useState(pokemon.name);
+    
+    
+    return (
+        <div className={`card ${pokemon.type}`}>
+            <h1>{pokemon.name}</h1>
+            <p>HP: {pokemon.hp}</p>
+            <img src={pokemon.img} alt={pokemon.name} />
+            <ul>
+                {pokemon.moves.map((move, index) => {
+                    if (index < 3) {
+                        return <li key={index}>{move.move.name}</li>;
+                    }
+                })}
+            </ul>
+            <form onSubmit={(e) => props.replace(e, props.index, pokemonName)}>
+                <input type="text" className="update-name" value={pokemonName} onChange={(e) => setPokemonName(e.target.value)} />
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    );
+}
+export default App;
